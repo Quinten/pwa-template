a251d9853f794a2797edf7af2365fb0b230112fb
Quinten Clause <quinten.clause@studioemma.eu>
2019-12-08 13:33:05 +0100

    Add basic template

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const path = require('path');
+
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+const config = require('./package');
+
+module.exports = (env, argv) => {
+
+    const devMode = argv.mode !== 'production';
+
+    let webpackConfig = {
+
+        entry: './src/index.js',
+
+        output: {
+            path: path.resolve(__dirname, 'pub'),
+            publicPath: '',
+            filename: 'index.js'
+        },
+
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/, // not really sur about this
+                    use: {
+                        loader: 'babel-loader'
+                    }
+                }
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                ...config,
+                template: 'src/index.html'
+            })
+        ],
+
+        devServer: {
+            contentBase: "./pub",
+            host: "0.0.0.0",
+            port: 3000
+        }
+    }
+
+    if (!devMode) {
+
+        webpackConfig.optimization = {
+            minimizer: [
+                new TerserPlugin({
+                    terserOptions: {
+                        output: {
+                            comments: false
+                        }
+                    }
+                })
+            ]
+        }
+    }
+
+    return webpackConfig;
+};
